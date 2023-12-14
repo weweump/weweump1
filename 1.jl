8faa4885cc41208a3a3e8b9704aed8ebb017df2b
weweump <144722891+weweump@users.noreply.github.com>
2023-12-14 19:55:11 +0300

    Add files via upload

---
+++ 1.jl
@@ -0,0 +1,45 @@
+using HorizonSideRobots
+robot = Robot("starts/task1.sit",animate = true)
+
+function all_mark!(robot::Robot)
+    num_nord = topleft_num!(robot,Nord)
+    num_west = topleft_num!(robot,West)   
+    side = Ost
+    markrow!(robot,side)
+    while !isborder(robot,Sud)
+        move!(robot,Sud)
+        side = inverse(side)
+        markrow!(robot,side)
+    end
+    topleft_num!(robot,Nord)
+    topleft_num!(robot,West)
+    goback!(robot,Sud,num_nord)
+    goback!(robot,Ost,num_west)
+end
+
+function goback!(robot,side,num_steps)
+    for x in range(0,num_steps-1)
+        move!(robot,side)
+    end
+end
+
+function topleft_num!(robot::Robot,side::HorizonSide)
+    num = 0
+    while !isborder(robot, side)
+        move!(robot,side)
+        num += 1
+    end
+    return num
+end
+
+function markrow!(robot::Robot,side::HorizonSide)
+    while !isborder(robot,side)
+        putmarker!(robot)
+        move!(robot,side)
+    end
+    putmarker!(robot)
+end
+
+inverse(side::HorizonSide) = HorizonSide(mod(Int(side)+2,4))
+
+all_mark!(robot)
